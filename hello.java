a43720266ea6e952318789393ad767beb2507d30
Aadinath16 <adi.dhamdhere@yahoo.com>
2021-01-06 01:55:05 -0800

    4th commit

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-class HelloWorld {
-	public static void main(String[] args) {
-		system.out.println("Hello, World");
-	}
+class hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
